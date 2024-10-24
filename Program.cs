fad15516901241103ba3d8e021d14fef7947282b
brunoholiveira <124120411+brunoholiveira@users.noreply.github.com>
2024-10-24 17:02:25 -0300

    parte#2 teste de mesa
    exercicio 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace testeDeMesa
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorPresente = 1000;
+            double taxaJuros = 5.30 / 100;
+            int periodoMes = 6;
+            //double percentualJuros = 2 / 100;
+
+            double valorFuturo = valorPresente * Math.Pow((1+taxaJuros), periodoMes);
+            
+            Console.WriteLine("O valor futuro é: " + valorFuturo.ToString("F2"));
+        }
+    }
+}
