5f6e5796d2e4ae4de560c73f00f69070b3553b86
brunoholiveira <124120411+brunoholiveira@users.noreply.github.com>
2024-11-06 14:50:23 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,23 @@
-﻿using System;
+﻿﻿using System;
 
 namespace testeDeMesa
 {
     public class Program
+{
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            double valorPresente = 1000;
-            double taxaJuros = 5.30 / 100;
-            int periodoMes = 6;
-            //double percentualJuros = 2 / 100;
+        double[] entradas = {1000, 5500, 12000};
+        double[] taxas = {0.03, 0.0248, 0.02};
 
-            double valorFuturo = valorPresente * Math.Pow((1+taxaJuros), periodoMes);
-            
-            Console.WriteLine("O valor futuro é: " + valorFuturo.ToString("F2"));
+        int meses = 8;
+        int dias = 10;
+        double n_periodos = meses + (dias / 30.0);
+
+        for (int i = 0; i < entradas.Length; i++)
+        {
+            double valorFuturo = entradas[i] * Math.Pow(1 + taxas[i], n_periodos);
+            Console.WriteLine($"Valor Futuro para entrada {entradas[i]} com taxa {taxas[i] * 100}%: {valorFuturo:F2}");
         }
     }
+}
 }
